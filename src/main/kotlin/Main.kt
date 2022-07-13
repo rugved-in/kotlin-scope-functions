3c3bde86199d30f5569e536f6416f63bc227c10b
rugved-i <rugved.inamdar@amla.io>
2022-07-13 12:16:53 +0530

    with scope function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,11 @@ class Person {
 }
 
 fun main(args: Array<String>) {
+
+    /*Scope function: 'with'
+    Property 1: Refer to context object by using 'this'
+    Property 2: The return value is the 'lambda result'
+    */
     val person = Person()
 
     with(person) {
