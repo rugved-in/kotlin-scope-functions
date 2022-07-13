2a6b20df2cfc70e8086a788a970ddeea7db5b6b0
rugved-i <rugved.inamdar@amla.io>
2022-07-13 12:56:51 +0530

    run scope function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,7 +9,10 @@ fun main(args: Array<String>) {
     Property 1: Refer to context object by using 'this'
     Property 2: The return value is the 'lambda result'
     */
-    val person: Person? = null
+    val person = Person().apply {
+        name = "Rugved"
+        age = 31
+    }
 
     val ageAfterFiveYears = person?.run {
         println(this.name)
